e0743eb35c493398269f2b8e7c4c860b8397fd60
tonovarela <tonovarela@live.com>
2024-06-17 20:22:18 -0600

    Fin de la seccion 11

--- src/modules/common/components/CustomModal.vue
+++ src/modules/common/components/CustomModal.vue
@@ -1,2 +1,27 @@
 <template>
+  <dialog class="modal" :open="open">
+    <div class="modal-box">
+      <div class="border-b border-blue-500">
+        <slot name="header"></slot>
+      </div>
+      <div class="my-5">        
+        <slot name="body"></slot>
+      </div>
+      <div class="border-t border-t-blue-500  pt-2">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </dialog>
+  <div v-if="open" class="modal-backdrop fixed top-0 left-0 z-10  bg-gray-500/50  w-screen h-screen"></div>
 </template>
+<script setup lang="ts">
+interface Props {
+  open: boolean;
+}
+
+
+withDefaults(defineProps<Props>(), {
+  open: true
+});
+
+</script>
--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -23,9 +23,28 @@
             </tbody>
         </table>
         <input-modal :open="modalOpen" title="Nuevo Proyecto" subtitle=" Dale un nombre único a tu proyecto"
-            placeholder="Ingrese el nombre del proyecto" @close="cerrarModal" @value="onNewProject" />
+            placeholder="Ingrese el nombre del proyecto" @close="cerrarModal" @value="onNewProject" />                    
+       <custom-modal  :open="customModalOpen">
+        <template #header>
+            <h1 class="text-3xl">Cabecera</h1>
+        </template>
+        <template #body>
+            <h1>Varela </h1>
+        </template>
+        <template #footer>
+            <div class="flex justify-end mt-5">
+                        <button class="btn mr-4" @click="customModalOpen=false">Close</button>
+                        <button type="submit"  @click="customModalOpen=false" class="btn btn-primary">Aceptar</button>
+                    </div>
+        </template>
+                    
+        </custom-modal>            
+
         <fab-button @click="modalOpen = true">
             <add-circle />
+        </fab-button>
+        <fab-button @click="customModalOpen = true" position="bottom-left">
+            <code-icon />
         </fab-button>
 
     </div>
@@ -34,10 +53,13 @@
 <script setup lang="ts">
 import FabButton from '@/modules/common/components/FabButton.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import CodeIcon from '@/modules/common/icons/CodeIcon.vue';
 import InputModal from '@/modules/common/components/InputModal.vue';
+import CustomModal from '@/modules/common/components/CustomModal.vue';
 import { ref } from 'vue';
 
 const modalOpen = ref(false);
+const customModalOpen = ref(false);
 
 const cerrarModal = () => {
     modalOpen.value = false;
